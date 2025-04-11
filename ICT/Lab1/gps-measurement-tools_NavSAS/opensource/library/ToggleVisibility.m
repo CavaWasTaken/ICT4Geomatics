a3d495628341bc308a48476e18499d0824d316ca
Lorenzo <cavallaro.lory@gmail.com>
2025-04-11 19:31:46 +0200

    Started Lab1 report

--- ICT/Lab1/gps-measurement-tools_NavSAS/opensource/library/ToggleVisibility.m
+++
@@ -1,9 +0,0 @@
-function ToggleVisibility(event)
-    % Toggle the visibility of the selected line
-    obj = event.Peer; % Get the line object
-    if strcmp(obj.Visible, 'on')
-        obj.Visible = 'off';
-    else
-        obj.Visible = 'on';
-    end
-end
